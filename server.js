c8a1179dddb36163353bae8540f74001995ef948
Gaetan Voyer-Perrault <gatesvp@gmail.com>
2011-04-18 00:30:32 -0500

    now with mongodb

--- server.js
+++ server.js
@@ -13,25 +13,32 @@ var port = 27017;
 var db = new Db('visits', new Server(host, port, {}));
 
 db.open(function(err, db) { 
-    http.createServer(function (req, res) {
- 
-       // get IP address and ts and query object
-        global.inData = { };
-        global.inData.ip = req.connection.remoteAddress;
-        global.inData.ts = new Date().valueOf();
-        global.inData.qs = require('url').parse(req.url, true);
+  http.createServer(function (req, res) {
 
-        db.collection('views', function(err, collection) { 
-            if (err) {
-                console.log('is error \n' + err);
-            }
+    // get IP address and ts and query object
+    global.inData = { };
+    global.inData.ip = req.connection.remoteAddress;
+    global.inData.ts = new Date().valueOf();
+    global.inData.qs = require('url').parse(req.url, true);
 
-            res.writeHead(200, {'Content-Type': 'text/plain'});
-            collection.insert(global.inData);
-            res.end("IP recorded");
-            //db.close();  // DO NOT CLOSE THE CONNECTION
-        }); 
-    }).listen(default_port); 
+    db.collection('views', function(err, collection) { 
+        if (err) {
+            console.log('is error \n' + err);
+        }
+
+        collection.insert(global.inData);
+
+        collection.find({}, {limit:5, sort:[ ['ts','desc'] ] }).toArray( function(err, docs) {
+          res.writeHead(200, {'Content-Type': 'text/plain'});
+          for(var i in docs){
+            res.write(JSON.stringify(docs[i]);
+            res.write("\n");
+          }
+          res.end("Last five");
+          //db.close();  // DO NOT CLOSE THE CONNECTION
+        });
+    }); 
+  }).listen(default_port); 
 });
 
 console.log('Server running on default port');
