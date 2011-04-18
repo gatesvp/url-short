36138a217d5e67410cd9c0cc5642f8d780a0de0d
Gaetan Voyer-Perrault <gatesvp@gmail.com>
2011-04-17 21:48:52 -0500

    first run

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+var default_port = process.env.PORT;
+
 var http = require('http');
 
 http.createServer( function(req, res) { 
   res.writeHead(200, {'Content-Type' : 'text/plain' });
   res.end('Hello World\n');
-}).listen();
+}).listen(default_port);
 
 console.log('Server running on default port');
 
