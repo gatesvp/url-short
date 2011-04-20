732c277e1b2d0eea21fc41cbf6be9abcf9e18a83
Gaetan Voyer-Perrault <gatesvp@gmail.com>
2011-04-20 00:15:15 -0500

    upgrade to express

--- server.js
+++ server.js
@@ -13,43 +13,18 @@ var host = 'localhost';
 var port = 27017;
 var db = new Db('visits', new Server(host, port, {}));
 
-db.open(function(err, db) { 
-  var app = express.createServer();
+//db.open(function(err, db) { 
 
-  app.use(app.router);
+app = express.createServer();
 
-  app.get('/', function (req, res, next) {
-
-    // get IP address and ts and query object
-    global.inData = { };
-    global.inData.ip = req.connection.remoteAddress;
-    global.inData.ts = parseInt(new Date().valueOf());
-    global.inData.qs = require('url').parse(req.url, true);
-
-    db.collection('views', function(err, collection) { 
-        if (err) {
-            console.log('is error \n' + err);
-        }
-
-        collection.insert(global.inData);
-
-        collection.find({}, {limit:5, sort:[ ['ts','desc'] ] }).toArray( function(err, docs) {
-//          res.writeHead(200, {'Content-Type': 'text/plain'});
-//          res.write("Last Five Requests\n");
-//          for(var i in docs){
-//            res.write(i + "\n");
-//            res.write(JSON.stringify(docs[i], null, '\t'));
-//            res.write("\n\n");
-//          }
-          jade.renderFile('views/index.jade', docs);
-//          res.end("");
-          //db.close();  // DO NOT CLOSE THE CONNECTION
-        });
-    }); 
+app.get('/',function(req,res){
+  var local_var = "I am a local var";
+  jade.renderFile('index.jade', {locals:{local_var:local_var}} ,function(err,html){
+    res.send(html);
   });
-
-  app.listen(default_port); 
 });
+
+app.listen(default_port); 
 
 console.log('Server running on default port');
 
