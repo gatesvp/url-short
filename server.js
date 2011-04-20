e6e38b0d4c3145d348b33948ef2c42cfb167d191
Gaetan Voyer-Perrault <gatesvp@gmail.com>
2011-04-20 01:02:31 -0500

    upgrade to express

--- server.js
+++ server.js
@@ -13,15 +13,73 @@ var host = 'localhost';
 var port = 27017;
 var db = new Db('visits', new Server(host, port, {}));
 
-//db.open(function(err, db) { 
+function NotFound(path) {
+  this.name = 'NotFound';
+  if(path){
+    Error.call(this, 'Cannot find ' + path);
+    this.path = path;
+  }
+  else{
+    Error.call(this, 'Not Found');
+  }
+  Error.captureStackTrace(this, arguments.callee);
+}
 
-app = express.createServer();
+NotFound.prototype.__proto__ = Error.prototype;
 
-app.get('/',function(err, req,res){
-  res.render('views/index.jade');
+db.open(function(err, db) { 
+  var app = express.createServer();
+
+  app.use(app.router);
+  app.use(function(req,res,next){
+    next(new NotFound(req.url));
+  });
+  app.set('views', __dirname + '/views');
+
+  app.error(function(err, req, res, next) {
+//    if (err instanceof NotFound) {
+      res.render('404.jade', { status : 404, error: err });
+//    }
+//    else {
+//      next(err);
+//    }
+  });
+
+  app.error(function(err, req, res){
+    res.render('500.jade', { status: 500, error: err });
+  });
+
+  app.get('/', function (req, res, next) {
+
+    // get IP address and ts and query object
+    global.inData = { };
+    global.inData.ip = req.connection.remoteAddress;
+    global.inData.ts = parseInt(new Date().valueOf());
+    global.inData.qs = require('url').parse(req.url, true);
+
+    db.collection('views', function(err, collection) { 
+        if (err) {
+            console.log('is error \n' + err);
+        }
+
+        collection.insert(global.inData);
+
+        collection.find({}, {limit:5, sort:[ ['ts','desc'] ] }).toArray( function(err, docs) {
+
+          jade.renderFile('views/index.jade', docs ,function(err,html){
+            res.send(html);
+          });
+
+          //db.close();  // DO NOT CLOSE THE CONNECTION
+        });
+    }); 
+  });
+
+  app.get('/404', function(req, res) { throw new NotFound(req.url); } );
+  app.get('/500', function(req, res) { next(new Error('keyboard cat!')); } );
+
+  app.listen(default_port); 
 });
-
-app.listen(default_port); 
 
 console.log('Server running on default port');
 
