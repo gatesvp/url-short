6042fb156babc2cb5ca5c1785aa5b89af4b87f4a
Gaetan Voyer-Perrault <gatesvp@gmail.com>
2011-04-20 02:41:06 -0500

    upgrade to express

--- server.js
+++ server.js
@@ -19,9 +19,6 @@ db.open(function(err, db) {
   app.set('view engine', 'jade');
 
   app.use(app.router);
-  app.use(function(req,res,next){
-    jade.renderFile('views/404.jade', function(err,html){ res.send(html); });
-  });
   app.use('/', express.errorHandler({ dump: true, stack: true }));
 
   app.get('/', function (req, res, next) {
