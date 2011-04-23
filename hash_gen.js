18461664d4a281b482c8e6c53f341501178a5cc2
Gaetan Voyer-Perrault <gatesvp@gmail.com>
2011-04-23 05:11:48 -0500

    URL shortener

---
+++ hash_gen.js
@@ -0,0 +1,38 @@
+var hash_gen = function(db){
+  var _current_increment = '';
+  var _hash_array = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+
+  db.collection('shortened', function(err, collection){
+    collection.find({}, {limit:1, sort:[ ['ts','desc'] ] }).toArray( function(err, docs) {
+      _current_increment = (docs.length > 0 ? docs[0]._id : 'a');
+    });
+  });
+
+  debase = function(x){
+    var l = _hash_array.length;
+    var sum = 0;
+
+    for(i = 0; i < x.length; i++){
+      sum += _hash_array.indexOf(x[i]) + (i * l);
+    }
+    return sum;
+  }
+  rebase = function(x){
+    l = _hash_array.length;
+    var res = _hash_array[x % l];
+
+    while( x / l >= 1){
+      x = x/l;
+      res += _hash_array[x % l];
+    }
+    return res;
+  }
+
+  return {
+    get_next : function(){
+      _current_increment = rebase(debase(_current_increment)+1);
+      return _current_increment;
+    }
+  };
+};
+
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require.paths.unshift('/home/node/express/support');
+require.paths.unshift(__dirname + 'hash_gen.js');
 
 var default_port = process.env.PORT;
 var pub = __dirname + '/public';
@@ -19,44 +20,6 @@ var host = 'localhost';
 var port = 27017;
 var db = new Db('visits', new Server(host, port, {}));
 
-var hash_gen = function(db){
-  var _current_increment = '';
-  var _hash_array = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-
-  db.collection('shortened', function(err, collection){
-    collection.find({}, {limit:1, sort:[ ['ts','desc'] ] }).toArray( function(err, docs) {
-      _current_increment = (docs.length > 0 ? docs[0]._id : 'a');
-    });
-  });
-
-  debase = function(x){
-    var l = _hash_array.length;
-    var sum = 0;
-
-    for(i = 0; i < x.length; i++){
-      sum += _hash_array.indexOf(x[i]) + (i * l);
-    }
-    return sum;
-  }
-  rebase = function(x){
-    l = _hash_array.length;
-    var res = _hash_array[x % l];
-
-    while( x / l >= 1){
-      x = x/l;
-      res += _hash_array[x % l];
-    }
-    return res;
-  }
-
-  return {
-    get_next : function(){
-      _current_increment = rebase(debase(_current_increment)+1);
-      return _current_increment;
-    }
-  };
-};
-  
 
 db.open(function(err, db) { 
 
