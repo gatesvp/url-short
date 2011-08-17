33b841348fbbd89be39929543068edacd3b5cafc
Gaetan Voyer-Perrault <gatesvp@gmail.com>
2011-08-17 05:01:33 -0500

    Fix default utils

--- mongo_utils.js
+++ mongo_utils.js
@@ -5,7 +5,7 @@ module.exports.generate_mongo_url = function(obj){
 
   obj.hostname = (obj.hostname || 'localhost');
   obj.port = (obj.port || 27017);
-  obj.db = (obj.db || 'test');
+  obj.db = (obj.db || 'visits');
 
   if(obj.username && obj.password){
     return "mongodb://" + obj.username + ":" + obj.password + "@" + obj.hostname + ":" + obj.port + "/" + obj.db + "?auto_reconnect=true";
