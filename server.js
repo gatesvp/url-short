e224162a2368391e7f6abeae74fe9f1fbc199ad4
Gaetan Voyer-Perrault <gatesvp@gmail.com>
2011-08-22 05:42:01 -0500

    Adding jqplot to use for history/stats.jade

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var sys = require('sys');
 var express = require('express');
 var jade = require('jade');
 var mongodb = require('mongodb');
+var shorten = require('./shorten');
 
 /* Configure DB connections */
 var mongourl = require('./mongo_utils').generate_mongo_url();
@@ -24,13 +25,25 @@ app.use(app.router);
 app.use(express.static(pub));
 app.use('/', express.errorHandler({ dump: true, stack: true }));
 
+app.get('/history/:stub', function(req, res, next) {
+  if(req.params.stub){
+    mongodb.connect(mongourl, function(err, conn) { 
+      conn.collection('shortened', function(err, collection) { 
+        collection.findOne({_id: req.params.stub}, function(err, data) {
+          res.render('stats', { short: data._id, outurl: data.url });
+          conn.close();
+        });
+      });
+    });
+  }
+});
+
 app.get('/:stub', function(req, res, next) { 
   require('./forwarding').process_url_forward(req, res, next, mongodb, mongourl); 
 });
 
 app.get('/', function(req, res, next){
   if(req.query['url']){
-    var shorten = require('./shorten');
     var urlin = shorten.fix_url(req.query['url']);
     if(req.query['type'] === 'json'){
       shorten.shorten_url(req, res, next, mongodb, mongourl, gen, urlin, true);
@@ -45,10 +58,8 @@ app.get('/', function(req, res, next){
 });
 
 app.post('/', function (req, res, next) {
-
-  /* Trim URL, prepend HTTP if necessary */
-  var urlin = require('./shorten').fix_url(req.body.urlin);
-  require('./shorten').shorten_url(req, res, next, mongodb, mongourl, gen, urlin, false);
+  var urlin = shorten.fix_url(req.body.urlin);
+  shorten.shorten_url(req, res, next, mongodb, mongourl, gen, urlin, false);
 
 });
 
